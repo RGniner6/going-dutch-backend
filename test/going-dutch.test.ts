ea4e4f7717f871f79a558571650de81f827d4684
Rahul <rahul.gururaj96@gmail.com>
2025-06-25 18:54:57 +1000

    Initial commit

---
+++ test/going-dutch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GoingDutch from '../lib/going-dutch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/going-dutch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GoingDutch.GoingDutchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
